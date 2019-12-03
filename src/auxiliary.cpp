2985d47020e8cd4f34ce610ce99f59d11232d36a
jakimpl <jakimpl@gmail.com>
2019-12-03 22:31:12 +0100

    Added: auxiliary functions

--- src/auxiliary.cpp
+++ src/auxiliary.cpp
@@ -1,5 +1,4 @@
 #include "auxiliary.h"
-#include "constants.h"
 
 sf::RectangleShape rectangleCreate(int x, int y, int w, int h, sf::Color color)
 {
@@ -20,4 +19,196 @@ void writeByte(std::ofstream* file, unsigned short var)
 {
 	char buffer = static_cast<char>(var);
 	file->write(&buffer, 1);
+}
+
+void deleteGameObjects(Game &game)
+{
+	for (size_t type = 0; type < OBJ_COUNT; ++type) {
+		for (size_t index = 0; index < game.level.objectList[type].size(); ++index) {
+			delete game.level.objectList[type][index];
+		}
+	}
+}
+
+void drawBoard(Game &game, sf::RenderWindow &window, Object::Position mousePosition)
+{
+	for (short y = 0; y < game.level.height; ++y) {
+		for (short x = 0; x < game.level.width; ++x) {
+			sf::Color outlineColor, fillColor;
+			Object::Position currentPosition;
+			currentPosition.setPosition(x, y);
+			if (currentPosition == mousePosition) {
+				outlineColor = (game.level.obstacles[currentPosition] ? lgray : dgray);
+				fillColor = (game.level.obstacles[currentPosition] ? yellow : lgray);
+			} else {
+				outlineColor = (game.level.obstacles[currentPosition] ? lgray : dgray);
+				fillColor = (game.level.obstacles[currentPosition] ? dgray : gray);
+			}
+
+			window.draw(rectangleCreate(OFFSET_X + TILE_SIZE * x, OFFSET_Y + TILE_SIZE * y, TILE_SIZE, TILE_SIZE, outlineColor));
+			window.draw(rectangleCreate(OFFSET_X + TILE_SIZE * x + OUTLINE_SIZE, OFFSET_Y + TILE_SIZE * y + OUTLINE_SIZE, TILE_SIZE - 2 * OUTLINE_SIZE, TILE_SIZE - 2 * OUTLINE_SIZE, fillColor));
+		}
+	}
+}
+
+void drawLasers(Game &game, sf::RenderWindow &window)
+{
+	drawLasers(game, window, true);
+	drawLasers(game, window, false);
+}
+
+void drawLasers(Game &game, sf::RenderWindow &window, bool blackLasers)
+{
+	for (size_t beamerIndex = 0; beamerIndex < game.level.objectList[OBJ_BEAMER].size(); ++beamerIndex) {
+		Beamer* beamer = (Beamer*) game.level.objectList[OBJ_BEAMER][beamerIndex];
+		size_t rays = beamer->laser.size();
+
+		for (size_t rayIndex = 0; rayIndex < rays; ++rayIndex) {
+			size_t size = beamer->laser[rayIndex].size();
+			sf::Vertex vertices[size];
+			std::copy(beamer->laser[rayIndex].begin(), beamer->laser[rayIndex].end(), vertices);
+			if (blackLasers) {
+				for (size_t node = 0; node < size; ++node) {
+					vertices[node].color = black;
+				}
+			}
+
+			window.draw(vertices, size, sf::LinesStrip, (blackLasers ? sf::BlendNone : sf::BlendAdd));
+		}
+	}
+}
+
+void drawGameObject(Game &game, sf::RenderWindow &window, Drag &drag, Object::Position mousePosition)
+{
+	bool shiftPressed = (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift));
+	for (size_t type = 0; type < OBJ_COUNT; ++type) {
+		for (size_t index = 0; index < game.level.objectList[type].size(); ++index) {
+			// Draw the sprite unless it's dragged
+			if (drag.position != game.level.objectList[type][index]->position) {
+				window.draw(game.level.objectList[type][index]->sprite);
+			}
+		}
+	}
+	if (game.editor.isActive() && !shiftPressed) {
+		if (!game.editor.editMode) {
+			game.editor.sprite.setPosition(mousePosition);
+			window.draw(game.editor.sprite);
+		}
+	} else {
+		// Draw the dragged element (if there is any)
+		if (!drag.position.isNull()) {
+			drag.sprite.setPosition(mousePosition);
+			window.draw(drag.sprite);
+		}
+	}
+}
+
+void gameEvents(Game &game, Ev &event, Drag &drag, Object::Position position)
+{
+	keyboardGlobalEvents(game, event);
+
+	bool shiftPressed = (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift));
+	if (game.editor.isActive() && !shiftPressed) {
+		keyboardEditorEvents(game, event);
+		mouseEditorEvents(game, event, position);
+	} else {
+		mouseGameEvents(game, event, drag, position);
+	}
+
+	if (game.level.event) {
+		game.calculateLasers();
+		game.level.event = false;
+	}
+}
+
+void keyboardGlobalEvents(Game &game, Ev &event)
+{
+	if (event.type == sf::Event::KeyPressed) {
+		switch (event.key.code) {
+		case Key::E: {
+			game.editor.turn(!game.editor.isActive());
+			break;
+		}
+		case Key::S: {
+			if (game.editor.isActive()) {
+				game.saveLevel("001");
+			}
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
+
+void keyboardEditorEvents(Game &game, Ev &event)
+{
+	if (event.type == sf::Event::KeyPressed) {
+		switch (event.key.code) {
+		case Key::Tab: {
+			game.editor.switchMode();
+			break;
+		}
+		case Key::Num1: {
+			game.editor.setObject(OBJ_BEAMER);
+			break;
+		}
+		case Key::Num2: {
+			game.editor.setObject(OBJ_DOT);
+			break;
+		}
+		case Key::Num3: {
+			game.editor.setObject(OBJ_MIRROR);
+			break;
+		}
+		case Key::Num4: {
+			game.editor.setObject(OBJ_BENDER);
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
+
+void mouseEditorEvents(Game &game, Ev &event, Object::Position mousePosition)
+{
+	if (game.editor.editMode) {
+		if (event.type == sf::Event::MouseButtonPressed) {
+			if (event.mouseButton.button == sf::Mouse::Left) {
+				game.level.event = game.level.rotateObject(mousePosition);
+			} else if (event.mouseButton.button == sf::Mouse::Right) {
+				game.level.event = game.level.changeObjectColor(mousePosition);
+			}
+		}
+	} else {
+		if (event.type == sf::Event::MouseButtonPressed) {
+			if (event.mouseButton.button == sf::Mouse::Left) {
+				game.level.event = game.level.addObject(game.editor.getObject(), mousePosition);
+			} else if (event.mouseButton.button == sf::Mouse::Right) {
+				game.level.event = game.level.removeObject(mousePosition);
+			}
+		}
+	}
+}
+
+void mouseGameEvents(Game &game, Ev &event, Drag &drag, Object::Position mousePosition)
+{
+	if (event.type == sf::Event::MouseButtonPressed) {
+		game.level.event = game.level.dragObject(drag, mousePosition);
+	}
+
+	if (event.type == sf::Event::MouseButtonReleased) {
+		// If the drag position is not null, move an object to the new location (if possible)
+		if (!drag.position.isNull()) {
+			game.level.event = game.level.moveObject(drag.position, mousePosition);
+		}
+
+		// Rotate an object if possible
+		if (drag.position == mousePosition) {
+			game.level.event = game.level.rotateObject(mousePosition);
+		}
+
+		drag.position.setNull();
+	}
 }
--- src/auxiliary.h
+++ src/auxiliary.h
@@ -3,6 +3,9 @@
 
 #include <fstream>
 #include <SFML/Graphics.hpp>
+#include "constants.h"
+#include "game.h"
+#include "object.h"
 
 static std::vector<sf::Texture*> textures;
 
@@ -10,5 +13,15 @@ extern sf::RectangleShape rectangleCreate(int x, int y, int w, int h, sf::Color 
 extern sf::Texture loadTexture(const std::string &filename);
 extern void readByte(std::ifstream *file, unsigned short &var);
 extern void writeByte(std::ofstream *file, unsigned short var);
+extern void deleteGameObjects(Game &game);
+extern void drawBoard(Game &game, sf::RenderWindow &window, Object::Position mousePosition);
+extern void drawLasers(Game &game, sf::RenderWindow &window, bool blackLasers);
+extern void drawLasers(Game &game, sf::RenderWindow &window);
+extern void drawGameObject(Game &game, sf::RenderWindow &window, Drag &drag, Object::Position mousePosition);
+extern void gameEvents(Game &game, Ev &event, Drag &drag, Object::Position position);
+extern void keyboardGlobalEvents(Game &game, Ev &event);
+extern void keyboardEditorEvents(Game &game, Ev &event);
+extern void mouseEditorEvents(Game &game, Ev &event, Object::Position mousePosition);
+extern void mouseGameEvents(Game &game, Ev &event, Drag &drag, Object::Position mousePosition);
 
 #endif
